e5f74488ad65ff65896ca8ecaad0335637de2eb4
Javoxir3008 <asadbekturakulovic@gmail.com>
2024-10-02 14:00:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let numbers = [0,5, 12, 3, 8, 1, 20,44];
+
+let min = Math.min(...numbers);
+let max = Math.max(...numbers);
+
+console.log("Минимальное число:", min);
+console.log("Максимальное число:", max);
